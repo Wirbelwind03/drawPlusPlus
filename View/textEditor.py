710464b26edd52086cbe8a7d7a897beb4d5ded73
WirbelWind03 <dylanmei19@gmail.com>
2024-10-25 13:18:54 +0200

    Changes

--- View/textEditor.py
+++ View/textEditor.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from tkinter import scrolledtext
+
 from .Resources.Widgets.customText import CustomText
 from .Resources.Widgets.textLineNumbers import TextLineNumbers
 
@@ -7,19 +9,26 @@ from .Resources.Widgets.textLineNumbers import TextLineNumbers
 class TextEditor(tk.Frame):
     def __init__(self, *args, **kwargs) -> None:
         tk.Frame.__init__(self, *args, **kwargs)
-        self.text = CustomText(self)
+        #self.text = CustomText(self)
+        self.text = tk.Text(self)
         self.vsb = tk.Scrollbar(self, orient="vertical", command=self.text.yview)
         self.text.configure(yscrollcommand=self.vsb.set)
         self.text.tag_configure("bigfont", font=("Helvetica", "24", "bold"))
-        self.linenumbers = TextLineNumbers(self, width=30)
-        self.linenumbers.attach(self.text)
+        # self.linenumbers = TextLineNumbers(self, width=30)
+        # self.linenumbers.attach(self.text)
 
-        self.vsb.pack(side="right", fill="y")
-        self.linenumbers.pack(side="left", fill="y")
-        self.text.pack(side="right", fill="both", expand=True)
+        # Configure the grid of the Text Editor widget
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0) 
 
-        self.text.bind("<<Change>>", self._on_change)
-        self.text.bind("<Configure>", self._on_change)
+        self.vsb.grid(row=0, column=1, sticky="ns")  # Fill vertically
+        # self.linenumbers.grid(row=0, column=0, sticky="ns") 
+        self.text.grid(row=0, column=0, sticky="nsew")  # Fill both horizontally and vertically
+
+
+        # self.text.bind("<<Change>>", self._on_change)
+        # self.text.bind("<Configure>", self._on_change)
 
     def _on_change(self, event):
         self.linenumbers.redraw()
