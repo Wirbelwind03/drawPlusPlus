438f83d520c718439278ed8f312bc64c385d735d
WirbelWind03 <dylanmei19@gmail.com>
2024-10-29 23:57:12 +0100

    Removed unecessary property

--- Model/canvasImage.py
+++ Model/canvasImage.py
@@ -1,46 +1,35 @@
 from PIL import Image, ImageTk
+import os
 
-from Core.Math.vector2 import Vector2
-from Core.Collision.aabb import AABB
+from DrawLibrary.Core.Math.vector2 import Vector2
+from DrawLibrary.Core.Collision.aabb import AABB
 
 class CanvasImage:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0, y=0) -> None:
         self.id = -1
         self.filePath = ""
         self.coordinates = Vector2(x, y)
-        self._width = 0
-        self._height = 0
         self.image = None
         self.photoImage = None
-        self.bbox = AABB(x, y)
+        self.bbox = None
 
-    def loadImage(self, filePath):
-        self.image = Image.open(filePath)
-        self.width, self.height = self.image.size
-        self.photoImage = ImageTk.PhotoImage(self.image)
+    def load(self, filePath: str) -> None:
+        try:
+            # Check if the file exists
+            if not os.path.exists(filePath):
+                raise FileNotFoundError(f"The file '{filePath}' does not exist.")
 
-    def cutImage(self, x1, y1, width, height):
+            self.image = Image.open(filePath)
+            self.photoImage = ImageTk.PhotoImage(self.image)
+            width, height = self.image.size
+            self.bbox = AABB(self.coordinates.x, self.coordinates.y, width, height)
+        except FileNotFoundError as e:
+            print(e)
+
+    def cut(self, x1, y1, width, height) -> None:
         new_img = self.image.convert("RGBA")
         mask = Image.new("RGBA", (width, height), (255, 255, 255, 0))
         new_img.paste(mask, (x1, y1))
 
         self.image = new_img
         self.photoImage = ImageTk.PhotoImage(new_img)
-
-    @property
-    def width(self):
-        return self._width
-    
-    @width.setter
-    def width(self, width):
-        self._width = width
-        self.bbox.width = self.width
-
-    @property
-    def height(self):
-        return self._height
-    
-    @height.setter
-    def height(self, height):
-        self._height = height
-        self.bbox.height = self.height
