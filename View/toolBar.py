a18906c9630563ce77ccbf96b49e93d303f1667d
Victor <victor.gavioli2004@gmail.com>
2024-11-19 11:12:59 +0100

    modification légère toolbar

--- View/toolBar.py
+++ View/toolBar.py
@@ -9,6 +9,7 @@ class ToolBar(tk.Frame):
 
         self.rowconfigure(0, minsize=30)
         self.rowconfigure(1, minsize=30)
+        self.rowconfigure(2, minsize=30)
 
         # Tool 1 - Pencil
         pencilButton = tk.Button(self, text="Pencil", height=1, width=12)
@@ -17,3 +18,7 @@ class ToolBar(tk.Frame):
         # Tool 2 - Eraser
         eraserButton = tk.Button(self, text="Eraser", height=1, width=12)
         eraserButton.grid(row=1, column=0)
+
+        # Tool 2 - Eraser
+        HighButton = tk.Button(self, text="Highlighter", height=1, width=12)
+        HighButton.grid(row=2, column=0)
