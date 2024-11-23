5235b8e65826bbc0b40107779f60b7012b27d752
WirbelWind03 <dylanmei19@gmail.com>
2024-11-23 16:11:47 +0100

    Changes

--- Controller/menuBarController.py
+++ Controller/menuBarController.py
@@ -6,24 +6,24 @@ class MenuBarController:
 
         menuFile = tk.Menu(self, tearoff=0)
         # Add button "File" in the menu bar
-        self.add_cascade(label="File", menu=menuFile)
+        self.view.add_cascade(label="File", menu=menuFile)
         # Ajoute les button dans le menu déroulant
         menuFile.add_command(label="New")
         menuFile.add_command(label="Open")
         menuFile.add_command(label="Save")
         # Add a separator
         menuFile.add_separator()
-        menuFile.add_command(label="Exit", command=self.master.quit)  # Option Quitter
+        menuFile.add_command(label="Exit", command=self.view.master.quit)  # Option Quitter
 
         menuEdit = tk.Menu(self, tearoff=0)
         # Add button "Edit" in the menu bar
-        self.add_cascade(label="Edit", menu=menuEdit)
+        self.view.add_cascade(label="Edit", menu=menuEdit)
         menuEdit.add_command(label="Cut")
         menuEdit.add_command(label="Copy")
         menuEdit.add_command(label="Paste")
 
         menuExecute = tk.Menu(self, tearoff=0)
         # Add button "Execution" in the mnu bar
-        self.add_cascade(label="Execution", menu=menuExecute)
+        self.view.add_cascade(label="Execution", menu=menuExecute)
         menuExecute.add_command(label="Execute")
         menuExecute.add_command(label="Compile C")
