bc57afc21dd37d57605e3dd9bb2a551c6efe7a1d
WirbelWind03 <dylanmei19@gmail.com>
2024-12-16 00:28:50 +0100

    Added center  rendering

--- Controller/debugCanvasController.py
+++ Controller/debugCanvasController.py
@@ -18,20 +18,30 @@ class DebugCanvasController:
         self.erase(canvasImage)
 
         bbox = canvasImage.bbox
-        debugCanvasEntity.bboxCanvasID = self.view.create_rectangle(bbox.min.x, bbox.min.y, bbox.max.x, bbox.max.y, outline="red", width=2)
+        debugCanvasEntity.canvasID.append(self.view.create_rectangle(bbox.min.x, bbox.min.y, bbox.max.x, bbox.max.y, outline="red", width=2))
         
-        debugCanvasEntity.textCanvasIDs.append(self.view.create_text(bbox.topLeft.x, bbox.topLeft.y - 10, text=f"({bbox.topLeft.x}, {bbox.topLeft.y})", fill="red"))
-        debugCanvasEntity.textCanvasIDs.append(self.view.create_text(bbox.topRight.x, bbox.topRight.y - 10, text=f"({bbox.topRight.x}, {bbox.topRight.y})", fill="red"))
-        debugCanvasEntity.textCanvasIDs.append(self.view.create_text(bbox.bottomLeft.x, bbox.bottomLeft.y + 10, text=f"({bbox.bottomLeft.x}, {bbox.bottomLeft.y})", fill="red"))
-        debugCanvasEntity.textCanvasIDs.append(self.view.create_text(bbox.bottomRight.x, bbox.bottomRight.y + 10, text=f"({bbox.bottomRight.x}, {bbox.bottomRight.y})", fill="red"))
+        debugCanvasEntity.canvasID.append(self.view.create_text(bbox.topLeft.x, bbox.topLeft.y - 10, text=f"({bbox.topLeft.x}, {bbox.topLeft.y})", fill="red"))
+        debugCanvasEntity.canvasID.append(self.view.create_text(bbox.topRight.x, bbox.topRight.y - 10, text=f"({bbox.topRight.x}, {bbox.topRight.y})", fill="red"))
+        debugCanvasEntity.canvasID.append(self.view.create_text(bbox.bottomLeft.x, bbox.bottomLeft.y + 10, text=f"({bbox.bottomLeft.x}, {bbox.bottomLeft.y})", fill="red"))
+        debugCanvasEntity.canvasID.append(self.view.create_text(bbox.bottomRight.x, bbox.bottomRight.y + 10, text=f"({bbox.bottomRight.x}, {bbox.bottomRight.y})", fill="red"))
 
-        debugCanvasEntity.textCanvasIDs.append(self.view.create_text((bbox.min.x + bbox.max.x) / 2, bbox.min.y - 10, text=f"{bbox.width}", fill="green"))
-        debugCanvasEntity.textCanvasIDs.append(self.view.create_text(bbox.max.x + 15, (bbox.min.y + bbox.max.y) / 2, text=f"{bbox.height}", fill="green"))
+        debugCanvasEntity.canvasID.append(self.view.create_text((bbox.min.x + bbox.max.x) / 2, bbox.min.y - 10, text=f"{bbox.width}", fill="green"))
+        debugCanvasEntity.canvasID.append(self.view.create_text(bbox.max.x + 15, (bbox.min.y + bbox.max.y) / 2, text=f"{bbox.height}", fill="green"))
+
+        self.drawCanvasImageCenter(canvasImage)
+
+    def drawCanvasImageCenter(self, canvasImage: CanvasImage):
+        debugCanvasEntity: DebugCanvasEntity = self.debugCanvas[canvasImage.id]
+
+        bbox = canvasImage.bbox
+        debugCanvasEntity.canvasID.append(self.view.create_line(bbox.topLeft.x, bbox.topLeft.y, canvasImage.center.x, canvasImage.center.y, fill="green"))
+        debugCanvasEntity.canvasID.append(self.view.create_line(bbox.topRight.x, bbox.topRight.y, canvasImage.center.x, canvasImage.center.y, fill="green"))
+        debugCanvasEntity.canvasID.append(self.view.create_line(bbox.bottomLeft.x, bbox.bottomLeft.y, canvasImage.center.x, canvasImage.center.y, fill="green"))
+        debugCanvasEntity.canvasID.append(self.view.create_line(bbox.bottomRight.x, bbox.bottomRight.y, canvasImage.center.x, canvasImage.center.y, fill="green"))
 
     def erase(self, canvasImage: CanvasImage):
         debugCanvasEntity: DebugCanvasEntity = self.debugCanvas[canvasImage.id]
 
-        self.view.delete(debugCanvasEntity.bboxCanvasID)
-        for textCanvasID in debugCanvasEntity.textCanvasIDs:
-            self.view.delete(textCanvasID)
-        debugCanvasEntity.textCanvasIDs.clear()
+        for canvasID in debugCanvasEntity.canvasID:
+            self.view.delete(canvasID)
+        debugCanvasEntity.canvasID.clear()
