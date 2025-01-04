5ef14f4347593d75e5cf8e5324a1146ca3d30280
WirbelWind03 <dylanmei19@gmail.com>
2025-01-04 23:52:39 +0100

    Added globals symbol functions and variables

---
+++ DrawScript/Core/globals.py
@@ -0,0 +1,12 @@
+GLOBAL_SYMBOLS_FUNCTIONS = {
+    "setColorRGB": 3,   #setColorRGB(r, g, b)
+    "drawCircle": 3,    #drawCircle(x, y, radius)
+    "drawSegment": 4,   #drawSegment(x0, y0, x1, y1)
+    "drawSquare": 2,    #drawSquare(x0, y0, x1, y1)
+    "drawPoint": 2,     #drawPoint(x, y)
+}
+
+GLOBAL_SYMBOLS_VARIABLES = [
+    "CANVAS_WIDTH"
+    "CANVAS_HEIGHT"
+]
