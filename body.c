344d6676481fb1b6eaf39393994fe25932aa07b5
WirbelWind03 <dylanmei19@gmail.com>
2025-01-04 15:54:38 +0100

    Added body C code to insert the drawings, variables, etc.

---
+++ body.c
@@ -0,0 +1,48 @@
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "utils.h"
+#include "shapes.h"
+
+// Constantes globales
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+
+int main(int argc, char *argv[]) {
+    // Start SDL
+    SDL_Start();
+    SDL_Window *window = CreateWindow();
+    SDL_Renderer *renderer = CreateRenderer(window);
+
+    // INSERT VARIABLES
+    bool running = true;
+    SDL_Event event;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Transparent
+    SDL_RenderClear(renderer);
+
+    // INSERT DRAWINGS
+
+    SDL_RenderPresent(renderer);
+
+    // Boucle principale
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // INSERT ANIMATIONS
+
+        SDL_Delay(16);
+    }
+
+    // Clean
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
