c6b650c5e6d43d53d9320f0d6275c3ff110698a8
WirbelWind03 <dylanmei19@gmail.com>
2025-01-05 00:33:21 +0100

    Added another function

--- DrawScript/Core/globals.py
+++ DrawScript/Core/globals.py
@@ -1,5 +1,6 @@
 GLOBAL_SYMBOLS_FUNCTIONS = {
     "setColorRGB": 3,   #setColorRGB(r, g, b)
+    "setColorRGBA": 4,  #setColorRGBA(r, g, b, a)
     "drawCircle": 3,    #drawCircle(x, y, radius)
     "drawSegment": 4,   #drawSegment(x0, y0, x1, y1)
     "drawSquare": 2,    #drawSquare(x0, y0, x1, y1)
