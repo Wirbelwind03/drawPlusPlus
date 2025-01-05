ac0bf4789251e28cb666db54025cd18b33dcc69d
WirbelWind03 <dylanmei19@gmail.com>
2025-01-05 07:30:27 +0100

    Tests

--- DrawLibrary/C/SDL2/main.c
+++ DrawLibrary/C/SDL2/main.c
@@ -1,7 +1,10 @@
 #include <SDL2/SDL.h>
+#include <SDL2_gfxPrimitives.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
+
+#include "cursor.h"
 #include "utils.h"
 #include "shapes.h"
 
@@ -16,8 +19,14 @@ int main(int argc, char *argv[]) {
     SDL_Renderer *renderer = CreateRenderer(window);
 
     // Variables principales
-    int centerX = 400, centerY = 400, radius = 50, numCircles = 5, gridX = 0;
-    double angle = 0, speed = 0.05;
+    int centerX = 300;
+    int centerY = 300;
+    int radius = 50;
+    int numCircles = 5;
+    int angle = 0;
+    float speed = 0.05;
+    bool isAnimating = true;
+    int gridX = 0;
     bool running = true;
     SDL_Event event;
 
@@ -25,12 +34,12 @@ int main(int argc, char *argv[]) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Transparent
     SDL_RenderClear(renderer);
 
-    for (int i = 0; i < numCircles; i++) {
-        double offsetAngle = angle + (i * (360.0 / numCircles));
-        int offsetX = centerX + (radius * 3) * cos(offsetAngle);
-        int offsetY = centerY + (radius * 3) * sin(offsetAngle);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Noir
-        drawCircle(renderer, offsetX, offsetY, radius);
+    Cursor* cursor1 = Cursor_Constructor(centerX, centerY);
+    for (int  i = 0; (i < numCircles); (i = (i + 1)))
+    {
+        float offsetX = (centerX + ((radius * 3) * cos((angle + (i * (360 / numCircles))))));
+        float offsetY = (centerY + ((radius * 3) * sin((angle + (i * (360 / numCircles))))));
+        circleRGBA(renderer, offsetX, offsetY, radius, 0, 0, 0, 255);
     }
 
 	while((gridX <= 600))
@@ -38,14 +47,17 @@ int main(int argc, char *argv[]) {
 		int gridY = 0;
 		while((gridY <= 600))
 		{
-            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Bleu
-			drawCircle(renderer, gridX, gridY, 5);
+            circleRGBA(renderer, gridX, gridY, 5, 0, 0, 255, 255);
 			gridY = gridY + 50;
 		}
 		gridX = gridX + 50;
 	}
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Bleu
-	drawCircle(renderer, 250, 250, 75);
+    Cursor_Move(cursor1, 400, 300);
+    Cursor_Rotate(cursor1, 90);
+    Cursor_DrawSegment(cursor1, renderer, 600, 600);
+    Cursor_DrawCircle(cursor1, renderer, 50);
+    Cursor_DrawRectangle(cursor1, renderer, 100, 50);
+    circleRGBA(renderer, 250, 250, 75, 0, 0, 255, 255);
 
     SDL_RenderPresent(renderer);
 
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include "cursor.h"
 #include "utils.h"
 #include "shapes.h"
 
@@ -16,42 +17,44 @@ int main(int argc, char *argv[]) {
     SDL_Renderer *renderer = CreateRenderer(window);
 
     int centerX = 300;
-	int centerY = 300;
-	int radius = 50;
-	int numCircles = 5;
-	int angle = 0;
-	float speed = 0.05;
-	bool isAnimating = true;
-	int gridX = 0;
-	// INSERT VARIABLES
+int centerY = 300;
+int radius = 50;
+int numCircles = 5;
+int angle = 0;
+float speed = 0.05;
+bool isAnimating = true;
+int gridX = 0;
+// INSERT VARIABLES
     bool running = true;
     SDL_Event event;
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Transparent
     SDL_RenderClear(renderer);
 
-    for (int  i = 0; (i < numCircles); (i = (i + 1)))
-	{
-		float offsetX = (centerX + ((radius * 3) * cos((angle + (i * (360 / numCircles))))));
-		float offsetY = (centerY + ((radius * 3) * sin((angle + (i * (360 / numCircles))))));
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		drawCircle(renderer, offsetX, offsetY, radius);
-	}
-
-	while((gridX <= 600))
-	{
-		int gridY = 0;
-		while((gridY <= 600))
-		{
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-			drawCircle(renderer, gridX, gridY, 5);
-			(gridY = (gridY + 50));
-		}
-		(gridX = (gridX + 50));
-	}
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	drawCircle(renderer, 250, 250, 75);
-	// INSERT DRAWINGS
+    Cursor* cursor1 = Cursor_Constructor(centerX, centerY);
+for (int  i = 0; (i < numCircles); (i = (i + 1)))
+{
+	float offsetX = (centerX + ((radius * 3) * cos((angle + (i * (360 / numCircles))))));
+	float offsetY = (centerY + ((radius * 3) * sin((angle + (i * (360 / numCircles))))));
+	circleRGBA(renderer, offsetX, offsetY, radius, 0, 0, 0, 255);
+}
+while((gridX <= 600))
+{
+	int gridY = 0;
+	while((gridY <= 600))
+{
+	circleRGBA(renderer, gridX, gridY, 5, 0, 0, 0, 255);
+	(gridY = (gridY + 50));
+}
+	(gridX = (gridX + 50));
+}
+Cursor_Move(cursor1, 400, 300);
+Cursor_Rotate(cursor1, 90);
+Cursor_DrawSegment(cursor1, renderer, 600, 600);
+Cursor_DrawCircle(cursor1, renderer, 50);
+Cursor_DrawRectangle(cursor1, renderer, 100, 50);
+circleRGBA(renderer, 250, 250, 75, 0, 0, 0, 255);
+// INSERT DRAWINGS
 
     SDL_RenderPresent(renderer);
 
