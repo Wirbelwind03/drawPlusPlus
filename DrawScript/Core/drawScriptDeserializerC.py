c11903b915e4932450cc8ae7dbca36a4887e0060
WirbelWind03 <dylanmei19@gmail.com>
2025-01-05 19:47:51 +0100

    Added lines for priting the drawings positions

--- DrawScript/Core/drawScriptDeserializerC.py
+++ DrawScript/Core/drawScriptDeserializerC.py
@@ -13,26 +13,23 @@ class DrawScriptDeserializerC:
         f = open("body.c", "r")
         code = f.read()
 
-        insert_globals = code.find("// INSERT GLOBALS")
         if self.CC != None:
-            globals = f'#define SCREEN_WIDTH {self.CC.view.winfo_width()}\n#define SCREEN_HEIGHT {self.CC.view.winfo_height()}\n\n'
+            globals = f'#define SCREEN_WIDTH {self.CC.view.winfo_width()}\n#define SCREEN_HEIGHT {self.CC.view.winfo_height()}\n'
         else:
-            globals = f'#define SCREEN_WIDTH 800\n#define SCREEN_HEIGHT 600\n\n'
-        code = code[:insert_globals] + globals + code[insert_globals:]
+            globals = f'#define SCREEN_WIDTH 800\n#define SCREEN_HEIGHT 600\n'
+        code = code.replace("// INSERT GLOBALS", globals)
 
-        insert_variables = code.find("// INSERT VARIABLES")
         variables = ""  
         for ast_node in self.ast_nodes:
             if ast_node["node_type"] == "var_declaration":
                 variables +=  self.deserialize_node_type(ast_node) 
-        code = code[:insert_variables] + variables + code[insert_variables:]
+        code = code.replace("// INSERT VARIABLES", variables)
 
-        insert_drawings = code.find("// INSERT DRAWINGS")
         drawings = ""
         for ast_node in self.ast_nodes:
             if ast_node["node_type"] != "var_declaration":
                 drawings +=  self.deserialize_node_type(ast_node) 
-        code = code[:insert_drawings] + drawings + code[insert_drawings:]
+        code = code.replace("// INSERT DRAWINGS", drawings)
         
         f.close()
 
@@ -69,8 +66,6 @@ class DrawScriptDeserializerC:
             return str(ast_node["value"])
         elif ast_node["node_type"] == "for_statement":
             return self.deserialize_for_statement(ast_node)
-        elif ast_node["node_type"] == "function_declaration":
-            return self.code[:self.line_test] + self.deserialize_function_declaration(ast_node) + self.code[self.line_test:]
         elif ast_node["node_type"] == "var_declaration":
             return self.deserialize_var_declaration(ast_node) + "\n"
         elif ast_node["node_type"] == "while_statement":
@@ -88,18 +83,23 @@ class DrawScriptDeserializerC:
         cursor_name = ast_node["cursor_name"]
         method = ast_node["method"]
         arguments = ast_node["arguments"]
-        if method == "move":
-            return f'Cursor_Move({cursor_name}, {arguments[0]["value"]}, {arguments[1]["value"]});\n'
-        elif method == "rotate":
-            return f'Cursor_Rotate({cursor_name}, {arguments[0]["value"]});\n'
-        elif method == "drawCircle":
-            return f'Cursor_DrawCircle({cursor_name}, renderer, {arguments[0]["value"]});\n'
-        elif method == "drawSquare":
-            return f'Cursor_DrawRectangle({cursor_name}, renderer, {arguments[0]["value"]}, {arguments[1]["value"]});\n'
-        elif method == "drawSegment":
-            return f'Cursor_DrawSegment({cursor_name}, renderer, {arguments[0]["value"]}, {arguments[1]["value"]});\n'
 
-        return ""
+        deserialized = ""
+        if method == "move":
+            deserialized += f'Cursor_Move({cursor_name}, {arguments[0]["value"]}, {arguments[1]["value"]});\n'
+        elif method == "rotate":
+            deserialized += f'Cursor_Rotate({cursor_name}, {arguments[0]["value"]});\n'
+        elif method == "drawCircle":
+            deserialized += f'Cursor_DrawCircle({cursor_name}, renderer, {arguments[0]["value"]});\n'
+            deserialized += f'fprintf(file, "%d,%d\\n", (int){cursor_name}->x, (int){cursor_name}->y);\n'
+        elif method == "drawSquare":
+            deserialized += f'Cursor_DrawRectangle({cursor_name}, renderer, {arguments[0]["value"]}, {arguments[1]["value"]});\n'
+            deserialized += f'fprintf(file, "%d,%d\\n", (int){cursor_name}->x, (int){cursor_name}->y);\n'
+        elif method == "drawSegment":
+            deserialized += f'Cursor_DrawSegment({cursor_name}, renderer, {arguments[0]["value"]}, {arguments[1]["value"]});\n'
+            deserialized += f'fprintf(file, "%d,%d\\n", (int){cursor_name}->x, (int){cursor_name}->y);\n'
+
+        return deserialized
 
     def deserialize_function_declaration(self, ast_node):
         params = ""
@@ -150,7 +150,6 @@ class DrawScriptDeserializerC:
     def deserialize_statements(self, ast_node):
         deserialize = ""
         for statement in ast_node["statements"]:
-            deserialize += "\t"
             deserialize += self.deserialize_node_type(statement)
         return deserialize
     
@@ -174,9 +173,21 @@ class DrawScriptDeserializerC:
 
         if callee in GLOBAL_SYMBOLS_FUNCTIONS:
             if callee == "drawCircle":
+                x, y, radius = arguments[0]["value"], arguments[1]["value"], arguments[2]["value"]
                 deserialized = f'circleRGBA(renderer, {arguments_deserialized}, {self.current_color[0]}, {self.current_color[1]}, {self.current_color[2]}, {self.current_color[3]});\n'
-                deserialized += f'fprintf(file, "%d,%d\\n", (int){arguments[0]["value"]}, (int){arguments[1]["value"]})'
+                deserialized += f'fprintf(file, "%d,%d\\n", (int){arguments[0]["value"]}, (int){arguments[1]["value"]});\n'
+                deserialized += self.saveDrawing(f'{x} - {radius}', f'{y} - {radius}', f'{radius} * 2 + 1', f'{radius} * 2 + 1')
             else:
                 print(callee)
+
+        return deserialized
+    
+    def saveDrawing(self, x, y, width, height):
+        deserialized = ""
+
+        deserialized += f'snprintf(filename, sizeof(filename), "drawing_%d.bmp", drawing_index);\n'
+        deserialized += f'drawing_index++;\n'
+        deserialized += f'savePartialScreenshot(renderer, filename, {x}, {y}, {width}, {height});\n'
+        deserialized += f'ClearCanvas(renderer, 255, 255, 255, 255)'
 
         return deserialized
