739a1f0f9840c28a55959ba44a1d6ab5b35573f0
WirbelWind03 <dylanmei19@gmail.com>
2025-01-05 07:29:39 +0100

    Added functions

--- DrawLibrary/C/Utils/cursor.h
+++ DrawLibrary/C/Utils/cursor.h
@@ -1,9 +1,30 @@
-#ifndef SHAPES_H
-#define SHAPES_H
+#ifndef CURSOR_H
+#define CURSOR_H
+
+#include <SDL2/SDL.h>
 
 typedef struct {
     int x;
     int y;
+    int angle;
+    int thickness;
+    int rgba[4];
 } Cursor;
+
+Cursor* Cursor_Constructor();
+
+void Cursor_Move(Cursor* cursor, int x, int y);
+
+void Cursor_Rotate(Cursor* cursor, int degrees);
+
+void Cursor_DrawCircle(Cursor* cursor, SDL_Renderer* renderer, int radius);
+
+void Cursor_DrawSegment(Cursor* cursor, SDL_Renderer* renderer, int x1, int y1);
+
+void Cursor_DrawRectangle(Cursor* cursor, SDL_Renderer* renderer, int width, int height);
+
+void Cursor_SetColor(Cursor* cursor, int r, int g, int b, int a);
+
+void Cursor_SetThickness(Cursor* cursor, int thickness);
 
 #endif
