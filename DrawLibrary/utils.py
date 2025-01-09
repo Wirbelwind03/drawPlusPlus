00726fa983c265b7ab638ab91b1cc39c6a335474
WirbelWind03 <dylanmei19@gmail.com>
2025-01-09 02:43:26 +0100

    Merge

--- DrawLibrary/utils.py
+++ DrawLibrary/utils.py
@@ -5,8 +5,8 @@ class Utils:
         pass
 
     @staticmethod
-    def RemoveFilesInDirectory(directoryPath: str):
+    def RemoveFilesInDirectory(directoryPath: str, ignore_files = []):
         for filename in os.listdir(directoryPath):
             file_path = os.path.join(directoryPath, filename)
-            if os.path.isfile(file_path):
+            if os.path.isfile(file_path) and filename not in ignore_files:
                 os.remove(file_path)
