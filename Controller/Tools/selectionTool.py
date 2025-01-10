32062568c61be41ba9b8b3dc4f28e88d76f71f38
WirbelWind03 <dylanmei19@gmail.com>
2025-01-10 06:08:58 +0100

    Refactoring

--- Controller/Tools/selectionTool.py
+++ Controller/Tools/selectionTool.py
@@ -36,28 +36,15 @@ class SelectionTool:
     def canvas(self):
         return self.SRCC.CC.view
 
-    def getClickedImage(self, mouseCoords):
+    def getClickedImage(self, mouseCoords: Vector2) -> bool:
         # Loop all the images present on the canvas
         for imageId, image in self.SRCC.CC.model.images.items():
             # Check if the mouse is inside the bounding box of the image
             if image.bbox.isInside(mouseCoords):
-                self.setSelectedImage(image)
-                return
+                self.SRCC.selectImage(image)
+                return True
             
-    def setSelectedImage(self, image: CanvasImage):
-        # Create the selection rectangle around the selected image
-        self.SRCC.setSelectionRectangle(SelectionRectangle.fromCoordinates(image.bbox.min.x, image.bbox.min.y, image.bbox.max.x, image.bbox.max.y), image)
-        self.SRCC.create()
-
-        # Activate the resize tool input in the tool bar
-        self.toolBar.selectionRectangleWidth.set(self.SRCC.selectionRectangle.attachedImage.bbox.width) # Update the width input in the resize tool bar
-        self.toolBar.selectionRectangleHeight.set(self.SRCC.selectionRectangle.attachedImage.bbox.height) # Update the height input in the resize tool bar
-        self.toolBar.widthInput.configure(state="normal") # Activate the width input to not be read only
-        self.toolBar.heightInput.configure(state="normal") # Activate the height input to not be read only
-
-        # Check the action to move since the cursor is inside the image
-        self.SRCC.setAction(SelectionRectangleAction.MOVE) # Set the action that the user can move the image
-        self.canvas.config(cursor="fleur") # Change the cursor look
+        return False
 
     #region Event
 
@@ -80,11 +67,11 @@ class SelectionTool:
         if self.SRCC.hasSelectionRectangle() and self.SRCC.selectionRectangle.isOutside(mouseCoords):
             self.SRCC.deSelect()
             # Check if the user has clicked on another image
-            self.getClickedImage(mouseCoords)
-            self.SRCC.handle_clipboard_cut_activation(True)
-            self.SRCC.handle_clipboard_copy_activation(True)
+            if self.getClickedImage(mouseCoords):
+                self.SRCC.handle_clipboard_cut_activation(True)
+                self.SRCC.handle_clipboard_copy_activation(True)
 
-        if self.SRCC.hasSelectionRectangle() and self.SRCC.getAction() != SelectionRectangleAction.NONE:
+        if self.SRCC.hasSelectionRectangle() and self.SRCC.action != SelectionRectangleAction.NONE:
             # Calculate the offset between mouse click and rectangle's position
             self.SRCC.on_button_press(event)
             return
@@ -92,32 +79,28 @@ class SelectionTool:
     def on_mouse_drag(self, event: tk.Event) -> None:
         mouseCoords = Vector2(event.x, event.y)
 
-        if self.SRCC.hasSelectionRectangle() and self.SRCC.getAction() != SelectionRectangleAction.NONE:
+        if self.SRCC.hasSelectionRectangle() and self.SRCC.action != SelectionRectangleAction.NONE:
             self.SRCC.on_mouse_drag(event)
             return
         
     def on_button_release(self, event: tk.Event) -> None:
         mouseCoords = Vector2(event.x, event.y)
         
-        if self.SRCC.hasSelectionRectangle() and self.SRCC.getAction() != SelectionRectangleAction.NONE:
+        if self.SRCC.hasSelectionRectangle() and self.SRCC.action != SelectionRectangleAction.NONE:
             self.SRCC.on_button_release(event)
             pass
-            #self.SRCC.setAction(SelectionRectangleAction.NONE)
 
     def on_delete(self, event: tk.Event) -> None:
-        if self.SRCC.hasSelectionRectangle():
-            self.SRCC.deleteSelectionRectangle()
+        self.SRCC.deSelect()
 
     def on_control_c(self, event: tk.Event) -> None:
-        if self.SRCC.hasSelectionRectangle() and self.SRCC.selectionRectangle.attachedImage:
-            self.__copiedCanvasImage = self.SRCC.selectionRectangle.attachedImage
+        self.SRCC.on_control_c(event)
 
     def on_control_v(self, event: tk.Event) -> None:
-        if self.SRCC.hasSelectionRectangle():
-            if (self.__copiedCanvasImage):
-                newCanvasImage = self.SRCC.CC.drawImage(self.__copiedCanvasImage, 0, 0)
-                self.SRCC.deSelect()
-                self.setSelectedImage(newCanvasImage)
+        self.SRCC.on_control_v(event)
+
+    def on_control_x(self, event: tk.Event) -> None:
+        self.SRCC.on_control_x(event)
 
     def on_left(self, event: tk.Event) -> None:
         if self.SRCC.hasSelectionRectangle():
