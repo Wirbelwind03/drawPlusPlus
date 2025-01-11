b2bcb0dcdc27399d4b0bbd0d2af32ba819cc40d6
WirbelWind03 <dylanmei19@gmail.com>
2025-01-11 06:50:36 +0100

    Added more shapes

--- DrawScript/Core/globals.py
+++ DrawScript/Core/globals.py
@@ -1,22 +1,36 @@
 GLOBAL_SYMBOLS_FUNCTIONS = {
-    "setColorRGB": 3,   #setColorRGB(r, g, b)
-    "setColorRGBA": 4,  #setColorRGBA(r, g, b, a)
-    "drawCircle": 3,    #drawCircle(x, y, radius)
-    "drawSegment": 4,   #drawSegment(x0, y0, x1, y1)
-    "drawRectangle": 4, #drawRectangle(x, y, width, height)
-    "drawPoint": 2,     #drawPoint(x, y)
+    "setColorRGB": 3,   #(r, g, b)
+    "setColorRGBA": 4,  #(r, g, b, a)
+    "drawCircle": 3,    #(x, y, radius)
+    "drawFilledCircle": 3,    #(x, y, radius)
+    "drawEllipse": 4,   #(x, y, rx, ry)
+    "drawFilledEllipse": 4,   #(x, y, rx, ry)
+    "drawRoundedRectangle": 5,   #(x, y, width, height, radius)
+    "drawBox": 4,       #(x, y, width, height)
+    "drawRoundedBox": 5,       #(x, y, width, height, radius)
+    "drawSegment": 4,   #(x0, y0, x1, y1)
+    "drawTriangle": 6,  #(x0, y0, x1, y1, x2, y2)
+    "drawRectangle": 4, #(x, y, width, height)
+    "drawPoint": 2,     #(x, y)
 }
 
 # Methods to draw with the cursor
 GLOBAL_SYMBOLS_CURSOR_FUNCTIONS = {
-    "move": 2,           # move(x, y)"
-    "rotate": 1,         # rotate(degrees)
-    "drawCircle": 1,     # drawCircle(radius)
-    "drawSegment": 1,    # drawSegment(length)
-    "drawRectangle": 2,  # drawRectangle(width, height)
-    "drawPoint": 0,      # drawPoint()
-    "setColor": 1,       # Nouvelle méthode
-    "setThickness": 1    # Nouvelle méthode
+    "move": 2,           # (x, y)"
+    "rotate": 1,         # (degrees)
+    "drawCircle": 1,     # (radius)
+    "drawFilledCircle": 1,    #(radius)
+    "drawEllipse": 2,   #(rx, ry)
+    "drawFilledEllipse": 2,   #(rx, ry)
+    "drawRoundedRectangle": 3,   #(width, height, radius)
+    "drawBox": 2,       #(width, height)
+    "drawRoundedBox": 3,       #(width, height, radius)
+    "drawSegment": 1,    # (length)
+    "drawTriangle": 4,   # (x0, y0, x1, y1)
+    "drawRectangle": 2,  # (width, height)
+    "drawPoint": 0,      # ()
+    "setColor": 1,       #  
+    "setThickness": 1    # 
 }
 
 GLOBAL_SYMBOLS_VARIABLES = {
